2fd208ca699fc4af2c70de8b913088f6fefcabae
Andres <andres.porfirio@gmail.com>
2023-04-18 17:50:05 -0300

    added upsert and refresh examples of usage

--- src/pages/Admin/Books/[id]/Chapters/[chid]/index.page.tsx
+++ src/pages/Admin/Books/[id]/Chapters/[chid]/index.page.tsx
@@ -5,7 +5,10 @@ import { useParams } from "react-router-dom";
 export default function AdminBooksChaptersShowPage() {
   const { id, chid } = useParams();
 
-  const { data, loading } = useDocument<Chapter>(`books/${id}/chapters`, chid!);
+  const { data, loading, upsert, refresh } = useDocument<Chapter>(
+    `books/${id}/chapters`,
+    chid!
+  );
 
   if (loading) return <div>Loading...</div>;
 
@@ -14,6 +17,25 @@ export default function AdminBooksChaptersShowPage() {
       <h1>Chapter Show</h1>
 
       <span>{JSON.stringify(data)}</span>
+
+      <button
+        onClick={async () => {
+          await upsert({
+            ...data,
+            title: "Random title upsert " + Math.random(),
+          });
+        }}
+      >
+        upsert
+      </button>
+
+      <button
+        onClick={async () => {
+          await refresh();
+        }}
+      >
+        refresh
+      </button>
     </div>
   );
 }
